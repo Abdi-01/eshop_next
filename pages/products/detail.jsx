9a36fca8fb0a258182ac09add94e4e53673629c8
Abdi-01 <abdialghi@gmail.com>
2022-07-14 11:10:01 +0700

    next

--- pages/products/detail.jsx
+++ pages/products/detail.jsx
@@ -1,9 +1,10 @@
+import axios from 'axios';
 import React from 'react';
 
 const Detail = (props) => {
 
     return <div>
-        <h1>Detail Page</h1>
+        <h1 className='fw-bold'>Detail Page {props.detail.name}</h1>
     </div>
 }
 
@@ -11,9 +12,11 @@ export const getServerSideProps = async (context) => {
     try {
         // console.log('Data COntext request', context);
         console.log('Data query request', context.query);
+        let res = await axios.get(`http://localhost:2023/products?id=${context.query.id}`)
+        console.log(res.data)
         return {
             props: {
-
+                detail: res.data[0]
             }
         }
     } catch (error) {
